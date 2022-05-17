d3afe6d6a6dc0506f9799fda2de48d877339c07e
Nathan Betts <nathanbetts@hotmail.co.uk>
2022-05-17 22:06:42 +0100

    improve space background

--- src/components/canvas/SpaceBackground.tsx
+++ src/components/canvas/SpaceBackground.tsx
@@ -1,7 +1,7 @@
 import { Stars } from "@react-three/drei";
 
 const SpaceBackground = () => {
-  return <Stars radius={10000} depth={100000} count={20000} factor={1000} />;
+  return <Stars radius={20000} depth={100000} count={20000} factor={1200} />;
 };
 
 export default SpaceBackground;
