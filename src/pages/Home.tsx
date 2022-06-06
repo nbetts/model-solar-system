f4e0bd14cf60a324912616c3240b9500ecf0d25e
Nathan Betts <nathanbetts@hotmail.co.uk>
2022-06-06 19:38:06 +0100

    improve startup rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,24 @@ import { Canvas } from "@react-three/fiber";
 import InfoPanels from "src/components/overlay/InfoPanels";
 import Scene from "src/components/canvas/Scene";
 import StartupModal from "src/components/overlay/StartupModal";
-import { Loader } from "@react-three/drei";
-import { Suspense } from "react";
-import { LoadingOverlay } from "@mantine/core";
+import { useProgress } from "@react-three/drei";
 
-const Home = () => (
-  <Suspense fallback={<LoadingOverlay visible />}>
-    <Canvas shadows={false} style={{ position: "absolute", width: "100%", height: "100%", backgroundColor: "#030302" }}>
-      <Scene />
-    </Canvas>
-    <InfoPanels />
-    <StartupModal />
-  </Suspense>
-);
+const Home = () => {
+  const progress = useProgress((state) => state.progress);
+  const loading = progress < 100;
+
+  return (
+    <>
+      <Canvas
+        shadows={false}
+        style={{ position: "absolute", width: "100%", height: "100%", backgroundColor: "#030302" }}
+      >
+        <Scene />
+      </Canvas>
+      {!loading && <InfoPanels />}
+      <StartupModal loading={loading} />
+    </>
+  );
+};
 
 export default Home;
